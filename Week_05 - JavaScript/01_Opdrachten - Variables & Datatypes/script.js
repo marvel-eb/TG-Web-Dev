d4546cc5c8db7f14f5cc175e0f37374757d1cf51
marvel-eb <ephrem44be@yahoo.com>
2021-05-31 18:01:13 +0200

    JavaScript - Just started
    Week_05 - JavaScript Assignment

---
+++ Week_05 - JavaScript/01_Opdrachten - Variables & Datatypes/script.js
@@ -0,0 +1,160 @@
+/* Opdracht 01.1: 
+=================*/
+
+// a
+//
+// let letOefening = "Let Tekst";
+//let letOefening = "Let Tekst Twee!";
+//console.log(letOefening); 
+
+
+// b
+//let letOefening = "Let Tekst";
+//console.log(letOefening);
+
+//let letOefening = "Let Tekst Twee!";
+//console.log(letOefening);
+
+// c
+
+//const constOefeing = "Const Tekst";
+//console.log(constOefeing);
+//
+//constOefeing = "Const Tekst Twee!";
+//console.log(constOefeing);
+
+//  d
+
+//varOefening = "Var Tekst";
+//console.log(varOefening);
+//
+//var varOefening = "Var Tekst Twee";
+//console.logo(varOefening);
+//
+//var varOefening = "Var Tekst Drie!";
+//console.log(varOefening);
+
+
+/* Opdracht 01.2: 
+=================*/
+
+//a)
+//let typeQuestion ="number";
+//console.log("This is a" + typeof typeQuestion + ", with the value" + typeQuestion)
+
+//b)
+
+// (i)
+//let typeQuestion ="true";
+//console.log("This is a" + typeof typeQuestion + ", with the value" + typeQuestion);
+
+// (ii)
+//let typeQuestion ="False";
+//console.log("This is a" + typeof typeQuestion + ", with the value" + typeQuestion)
+
+
+// (iii)
+//let typeQuestion ="Undefined";
+//console.log("This is a" + typeof typeQuestion + ", with the value" + typeQuestion)
+
+// (iv)
+//let typeQuestion ="22";
+//console.log("This is a" + typeof typeQuestion + ", with the value" + typeQuestion)
+
+// (v)
+//let typeQuestion ="22n";
+//console.log("This is a" + typeof typeQuestion + ", with the value" + typeQuestion)
+
+// (vi)
+//let typeQuestion ="Symbole()";
+//console.log("This is a" + typeof typeQuestion + ", with the value" + typeQuestion)
+
+// (vii)
+//let typeQuestion ="null";
+//console.log("This is a" + typeof typeQuestion + ", with the value" + typeQuestion)
+
+// (viii)
+//let typeQuestion ="{}";
+//console.log("This is a" + typeof typeQuestion + ", with the value" + typeQuestion)
+
+// (xi)
+//let typeQuestion ="new Object()";
+//console.log("This is a" + typeof typeQuestion + ", with the value" + typeQuestion);
+
+// (x)
+//let typeQuestion ="function calculateSomething(){}";
+//console.log("This is a" + typeof typeQuestion + ", with the value" + typeQuestion);
+
+
+
+/* Opdracht 1.3: 
+=================*/
+
+//let someNumber = 5;
+//let anotherNumber = 5; // "5"
+//console.log(someNumber + anotherNumber)
+//console.log(someNumber = anotherNumber); // 5
+//console.log(someNumber === anotherNumber); // true
+//console.log(someNumber != anotherNumber); //  false
+//console.log(someNumber !== anotherNumber); // false
+ 
+
+
+/* Opdracht 01.4: 
+=================*/
+
+//a)
+//let increment = 1;
+//console.log(++increment); // 2
+//console.log(increment++);// 2
+
+//b)
+//let increment = 1;
+//console.log(increment++);//1
+//console.log(++increment); //3
+
+//c)
+//let modulo = 24 % 10;
+//console.log(modulo); // 4
+
+//let division = 24 / 10;
+//console.log(division); // 2.4
+
+//d)
+//let someNumber = "Tekst";
+//console.log(! someNumber); // false
+
+//e)
+//console.log(! 5); // false
+//console.log(! false); // true
+//console.log(! Boolean); // false
+//console.log(! "false)"; // Uncaught SyntaxError: missing ) after argument list
+//console.log(! true); // false
+//console.log(! "true"); // false
+//console.log(! null); // true
+//console.log(! undefined); // true
+
+
+//f) ?
+//g) ?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
