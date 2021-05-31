08e3f944116ef3a7d052823c3bdb20a3465d440f
marvel-eb <ephrem44be@yahoo.com>
2021-06-01 00:03:39 +0200

    Done
    JS - Opdracht 2 is done

---
+++ Week_05 - JavaScript/02_Opdrachten - String Methods/script.js
@@ -0,0 +1,44 @@
+/* Opdracht 02.1: 
+=================*/
+
+// let someKittens = "De poes van de buuren heeft kittens gehad! Hij vraagt of wij nog kittens willen?";
+
+// a)
+//let someKittens = "De poes van de buuren heeft kittens gehad!";
+//console.log(someKittens);
+//someKittens = " Hij vraagt of wij nog kittens willen?";
+//console.log(someKittens);
+
+// b), c), d)
+//let kittens = "Zo’n geweldig nieuws verdient upper case.";
+//let index = 5;
+//KITTENS = "Zet someKittens om naar hoofdletters.";
+//index = 6;
+//console.log(`The character at index ${index} is ${kittens.charAt(index)}`);
+
+/* Opdracht 02.2: 
+=================*/
+
+//let str = "AdiLove";
+//console.log(str.substring(1, 4)); // diL
+
+
+/* Opdracht 2.3: 
+=================*/
+
+//let someKittens = "Die nieuwe kittens zijn zo schattig!";
+//
+//let words = someKittens.split(' ');
+//console.log(words[2]); // kittens
+//
+//
+//const chars = someKittens.split('');
+//console.log(chars[4]); // n
+//
+//
+//const strCopy = someKittens.split();
+//console.log(strCopy); //Array [ "Die nieuwe kittens zijn zo schattig!" ]
+//
+//                    // 0: "Die nieuwe kittens zijn zo schattig!"
+//
+//                    // length: 1
