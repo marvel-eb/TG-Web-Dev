598c605c21b85befd40f901cd4d263c62ec80185
marvel-eb <ephrem44be@yahoo.com>
2021-06-01 21:58:03 +0200

    JS  - added

---
+++ Week_05 - JavaScript/03_Opdrachten - Conditions/script.js
@@ -0,0 +1,189 @@
+/* Opdracht 03.1: 
+=================*/
+//c, d)
+//
+//let x = 4;
+//let y = 8;
+//if (x == 4) {
+//  console.log("Result!");
+//} else {
+//}
+
+//e)
+
+let x = 4;
+let y = 8;
+if (x == 4) {} else {
+  console.log("Result!");
+}
+
+//f)???
+
+//let value = 1;
+////g)
+//if (value > 5 || value< 10) { 
+//console.log("greater than 5");
+//console.log("less than 10");
+//}
+////h)
+//else if (value > 11 || value< 20){
+//
+//console.log("greater than 11");
+//console.log("less than 20");
+//}
+//
+////i)
+//else if (value > 21 || value < 23){
+//
+//console.log("greater than 21");
+//console.log("less than 23");
+//}
+//
+////j)
+//else if (value > 35 || value < 35){
+//
+//console.log("greater than 35");
+//console.log("less than 35");
+//}
+////k)
+//
+//else {
+//   console.log("b/n 40 & 45");
+//}
+
+
+/* Opdracht 03.2: 
+=================*/
+
+
+//a),  b)
+// Method 1 //
+
+//var x = 2;
+//x = 3;
+//x = 4;
+//x = 5;
+//x = 20;
+//if (x == 3)
+//  console.log('x equal to 3');
+//else if (x > 4 || x > 11 || x < 3) {
+//  console.log("greater than 4");
+//  console.log("greater than 11");
+//  console.log("less than 3");
+//} else {
+//  console.log("There is Nothing");
+//}
+
+
+// Method 2 //
+
+//let x = 3;
+//if (x == 3)
+//  console.log('x equal to 3');
+//else if (x > 4 || x > 11 || x < 3) {
+//  console.log("greater than 4");
+//  console.log("greater than 11");
+//  console.log("less than 3");
+//} else {
+//  console.log("b/n 40 & 45");
+//}
+
+
+//c)
+
+//let value = 01;
+//if (value == 02) {
+//  console.log("Feb");
+//} else if (value == 03) {
+//  console.log("Mar");
+//} else if (value == 04) {
+//  console.log("Apr");
+//} else if (value == 05) {
+//  console.log("May");
+//} else if (value == 06) {
+//  console.log("Jun");
+//} else if (value == 07) {
+//  console.log("Jul");
+//} else if (value == 08) {
+//  console.log("Aug");
+//} else if (value == 09) {
+//  console.log("Sep");
+//}else if (value == 10) {
+//  console.log("Oct");
+//}
+//else if (value == 11) {
+//  console.log("Nov");
+//} else if (value == 12) {
+//  console.log("Dec");
+//} else if (value == 01) {
+//  console.log("Jan");
+//} else {
+//  console.log("Geen Geldige Maand");
+//}
+
+
+//d)
+
+//let value = 01;
+//switch (value = 01) {
+//  case 02:
+//    value = "Feb";
+//    console.log("Feb");
+//    break;
+//  case 03:
+//    value = "Mar";
+//    console.log("Mar");
+//    break;
+//  case 04:
+//    value = "Apr";
+//    console.log("Apr");
+//    break;
+//  case 05:
+//    value = "May";
+//    console.log("May");
+//    break;
+//  case 06:
+//    value = "Jun";
+//    console.log("Jun");
+//    break;
+//  case 07:
+//    value = "Jul";
+//    console.log("Jul");
+//    break;
+//  case 08:
+//    value = "Aug";
+//    console.log("Aug");
+//  case 09:
+//    value = "Sep";
+//    console.log("Sep");
+//  case 10:
+//    value = "Oct";
+//    console.log("Oct");
+//  case 11:
+//    value = "Nov";
+//    console.log("Nov");
+//  case 12:
+//    value = "Dec";
+//    console.log("Dec");
+//  case 01:
+//    value = "Jan";
+//    console.log("Jan");
+//  default:
+//    console.log("Geen Geldige Maand");
+//}
+
+
+//e) ???
+
+//let x = 3;
+//let results;
+//
+//if (x<=4 && x>=0) {
+//results = 2;
+//}
+//
+//else {
+//    results = 5;
+//}
+//console.log(results);
+/
