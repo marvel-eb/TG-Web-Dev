b8de287ae2aa3c7ede0822e94b49a57ba260a4d6
marvel-eb <ephrem44be@yahoo.com>
2021-06-03 09:45:19 +0200

    JS - Function in progress

---
+++ Week_05 - JavaScript/04_Functions/script.js
@@ -0,0 +1,124 @@
+/* Opdracht 04.1: 
+=================*/
+
+// a)
+//function myFunction() {
+//console.log("Helo World");
+//}
+//
+//// b)
+//function changeMyColor(myNewColor) {
+//  var element = document.getElementById('ephrem');
+//  element.style.color = myNewColor;
+//}
+
+
+// c)  ??????????????? -- must be checked again
+
+//let input = document.getElementById('ephrem-2')
+//function getElement() {
+//  console.log()
+//}
+
+
+// d)
+//function loveGenerator (beautiful, ugly) {
+//  const love = "Amazing Love  with beautiful $loveGenerator} girl";
+//  return beautiful * 2;
+//}
+//const result = loveGenerator(1);
+// console.log(result);
+
+
+/* Opdracht 04.2: 
+=================*/
+
+/*
+// a)
+// i)
+ function myFunction() {
+  var x = document.getElementsByClassName("homework-4");
+  x[0].innerHTML = "Hello World!";
+}
+
+// ii)
+function myFunction() {
+  var x = document.getElementsByTagName("p");
+  document.getElementById("homework-2").innerHTML = x[1].innerHTML;
+}
+
+//iii)
+function myFunction() {
+  document.querySelector(".homework-5").style.backgroundColor = "green";
+}
+// iv)
+function myFunction() {
+  var x, i;
+  x = document.querySelectorAll(".homework-3");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.backgroundColor = "green";
+  }
+}
+
+// v)
+function myFunction() {
+  document.getElementById("homework-4").innerHTML = "woow Paragraph changed!";
+}
+
+
+// vi)
+function myFunction() {
+  var x = document.getElementById("myBtn").innerText;
+  document.getElementById("content").innerHTML = x;
+}
+
+*/
+
+
+/* Opdracht 4.3: 
+=================*/
+
+
+// SWITCH + IF / ELSE
+
+//
+//let day = "fryday";
+//
+//switch (day) {
+//  case "monday":
+//    console.log("hello world");
+//    break;
+//  case "sunday":
+//    console.log("libdash kidistye");
+//    break;
+//  case "fryday":
+//    console.log("astebatalehu");
+//    break;
+//  default:
+//    console.log("invalid");
+//
+//}
+//
+//
+//if (day === "sunday") {
+//
+//  console.log("hello world");
+//} else if (day === "monday") {
+//  console.log("libdash kidistye");
+//
+//} else if (day === "fryday") {
+//
+//  console.log("ke canada metesh astebatalehu, ebedashalehu zekzike kidia shermuta");
+//
+//
+//} else {
+//  console.log("invalid");
+//}
+
+
+// FUNCTIONS
+
+//const width = 10;
+//const height = 6;
+//const area =  width * height;
+//console.log(area); // Output: 60
