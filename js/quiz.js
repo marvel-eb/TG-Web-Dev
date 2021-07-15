a1db5e6743d28daffb07f7ed68a74e8b598c20de
marvel-eb <ephrem44be@yahoo.com>
2021-07-15 18:34:22 +0200

    pages added

---
+++ js/quiz.js
@@ -0,0 +1,12 @@
+
+function sidebarMenu_open() {
+  document.getElementById("main").style.marginLeft = "25%";
+  document.getElementById("mySidebar").style.width = "25%";
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("openNav").style.display = 'none';
+}
+function sidebarMenu_close() {
+  document.getElementById("main").style.marginLeft = "0%";
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("openNav").style.display = "inline-block";
+}
