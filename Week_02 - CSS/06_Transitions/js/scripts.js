94d5c599f7e25027ec79ba5750b0981c9b40a89e
marvel-eb <ephrem44be@yahoo.com>
2021-07-15 23:40:38 +0200

    Update

--- Week_02 - CSS/06_Transitions/js/scripts.js
+++ Week_02 - CSS/06_Transitions/js/scripts.js
@@ -1,3 +1,4 @@
+//Sidebar Collapsable Menu
 
 function sidebarMenu_open() {
   document.getElementById("main").style.marginLeft = "25%";
@@ -5,8 +6,10 @@ function sidebarMenu_open() {
   document.getElementById("mySidebar").style.display = "block";
   document.getElementById("openNav").style.display = 'none';
 }
+
 function sidebarMenu_close() {
   document.getElementById("main").style.marginLeft = "0%";
   document.getElementById("mySidebar").style.display = "none";
   document.getElementById("openNav").style.display = "inline-block";
 }
+// End of Scripting //
