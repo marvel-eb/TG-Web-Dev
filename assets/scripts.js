d6b5045da011d001c7fef0b0754714cbd80863cc
marvel-eb <ephrem44be@yahoo.com>
2021-07-20 00:47:11 +0200

    update

---
+++ assets/scripts.js
@@ -0,0 +1,15 @@
+//Sidebar Collapsable Menu
+
+function menu_open() {
+  document.getElementById("main").style.marginLeft = "25%";
+  //  document.getElementById("mySidebar").style.width = "25%";
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("openNav").style.display = 'none';
+}
+
+function menu_close() {
+  document.getElementById("main").style.marginLeft = "0%";
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("openNav").style.display = "inline-block";
+}
+// End of Scripting //
