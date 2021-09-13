84a1c9ed5d76d83b14dbb05e56611bb2617b69ca
Ephrem B <ephrem44be@yahoo.com>
2021-09-13 14:13:31 +0200

    update

--- Week_10 - 16 [ React ]/eCommerceApp_React/src/firebase/config.js
+++ Week_10 - 16 [ React ]/eCommerceApp_React/src/firebase/config.js
@@ -1,22 +1,11 @@
-// export const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-// };
-
-// Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: "AIzaSyC8dqhLVtfCC1fHOi7dZ-YylaP5c6c5XiY",
-  authDomain: "reactshop-70094.firebaseapp.com",
-  databaseURL: "https://reactshop-70094-default-rtdb.firebaseio.com",
-  projectId: "reactshop-70094",
-  storageBucket: "reactshop-70094.appspot.com",
-  messagingSenderId: "482966608572",
-  appId: "1:482966608572:web:03dbeaaf8661bc34934eb9"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 
